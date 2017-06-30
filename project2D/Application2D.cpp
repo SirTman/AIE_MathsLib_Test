80f4734e6176f16acdbe728c4940ffb249d7d8d1
AIE\s171365 <theo.inc.au@gmail.com>
2017-06-30 17:11:37 +0930

    Stuff

--- project2D/Application2D.cpp
+++ project2D/Application2D.cpp
@@ -2,6 +2,13 @@
 #include "Texture.h"
 #include "Font.h"
 #include "Input.h"
+#include <Vector2.h>
+#include <Vector3.h>
+#include <Vector4.h>
+#include <Matrix2.h>
+#include <Matrix3.h>
+#include <Matrix4.h>
+#include <iostream>
 
 Application2D::Application2D() {
 
@@ -14,17 +21,30 @@ Application2D::~Application2D() {
 bool Application2D::startup() {
 	
 	m_2dRenderer = new aie::Renderer2D();
+	m_TankRenderer = new aie::Renderer2D();
+
 
 	m_texture = new aie::Texture("./textures/numbered_grid.tga");
 	m_shipTexture = new aie::Texture("./textures/ship.png");
 
-	m_font = new aie::Font("./font/consolas.ttf", 32);
+	m_tankBATexture = new aie::Texture("./textures/barrelGreen.png");
+	m_tankTexture = new aie::Texture("./textures/tankGreen.png");
 
+	m_font = new aie::Font("./font/consolas.ttf", 32);
 	m_audio = new aie::Audio("./audio/powerup.wav");
 
 	m_cameraX = 0;
 	m_cameraY = 0;
+	
+	m_tankX = 0;
+	m_tankY = 0;
+	//m_tankX = m_tankMatrix.m31;
+	//m_tankY = m_tankMatrix.m32;
+
+	m_tankR = 0;
+
 	m_timer = 0;
+
 
 	return true;
 }
@@ -35,6 +55,11 @@ void Application2D::shutdown() {
 	delete m_font;
 	delete m_texture;
 	delete m_shipTexture;
+	
+	delete m_tankBATexture;
+	delete m_tankTexture;
+
+	delete m_TankRenderer;
 	delete m_2dRenderer;
 }
 
@@ -47,20 +72,43 @@ void Application2D::update(float deltaTime) {
 
 	// use arrow keys to move camera
 	if (input->isKeyDown(aie::INPUT_KEY_UP))
-		m_cameraY += 500.0f * deltaTime;
+	{
+		//m_tankY -= 0.1f;
+		m_tankY -= 0.5f;
+		m_tankMatrix.m32 = m_tankY;
+	}
+	if (input->isKeyDown(aie::INPUT_KEY_DOWN)) 
+	{
+		m_tankY += 0.5f;
+		m_tankMatrix.m32 = m_tankY;
+	}
 
-	if (input->isKeyDown(aie::INPUT_KEY_DOWN))
-		m_cameraY -= 500.0f * deltaTime;
+	if (input->isKeyDown(aie::INPUT_KEY_LEFT)) 
+	{
+		
+		m_tankR -= 1.0f;
+		m_tankMatrix.setRotateZ(m_tankR);
+	}
+		
 
-	if (input->isKeyDown(aie::INPUT_KEY_LEFT))
-		m_cameraX -= 500.0f * deltaTime;
-
-	if (input->isKeyDown(aie::INPUT_KEY_RIGHT))
-		m_cameraX += 500.0f * deltaTime;
+	if (input->isKeyDown(aie::INPUT_KEY_RIGHT)) 
+	{
+		m_tankR += 1.0f;
+		m_tankMatrix.setRotateZ(m_tankR);
+		
+		
+	}
+		
 
 	// example of audio
 	if (input->wasKeyPressed(aie::INPUT_KEY_SPACE))
-		m_audio->play();
+	{
+		/*
+		std::cout << m_tankMatrix.m11 + " " + m_tankMatrix.m12 + " " + m_tankMatrix.m13 + "\n";
+		std::cout << m_tankMatrix.m21 + " " + m_tankMatrix.m22 + " " + m_tankMatrix.m23 + "\n";
+		std::cout << m_tankMatrix.m31 + " " + m_tankMatrix.m32 + " " + m_tankMatrix.m33 + "\n";
+		*/
+	}
 
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
@@ -77,36 +125,32 @@ void Application2D::draw() {
 
 	// begin drawing sprites
 	m_2dRenderer->begin();
+	
 
-	// demonstrate animation
-	m_2dRenderer->setUVRect(int(m_timer) % 8 / 8.0f, 0, 1.f / 8, 1.f / 8);
-	m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);
 
 	// demonstrate spinning sprite
-	m_2dRenderer->setUVRect(0,0,1,1);
-	m_2dRenderer->drawSprite(m_shipTexture, 600, 400, 0, 0, m_timer, 1);
-
-	// draw a thin line
-	m_2dRenderer->drawLine(300, 300, 600, 400, 2, 1);
-
-	// draw a moving purple circle
-	m_2dRenderer->setRenderColour(1, 0, 1, 1);
-	m_2dRenderer->drawCircle(sin(m_timer) * 100 + 600, 150, 50);
-
-	// draw a rotating red box
-	m_2dRenderer->setRenderColour(1, 0, 0, 1);
-	m_2dRenderer->drawBox(600, 500, 60, 20, m_timer);
-
-	// draw a slightly rotated sprite with no texture, coloured yellow
-	m_2dRenderer->setRenderColour(1, 1, 0, 1);
-	m_2dRenderer->drawSprite(nullptr, 400, 400, 50, 50, 3.14159f * 0.25f, 1);
 	
+	//m_2dRenderer->drawSprite(m_tankBATexture, m_tankX, m_tankY, 0, 0, m_timer, 1);
+	//m_2dRenderer->drawSprite(m_tankTexture, 0, 0, m_tankX, m_tankY, m_tankR, 1);
+	
+	m_2dRenderer->setUVRect(0, 0, 1, 1);
+	m_2dRenderer->drawSpriteTransformed3x3(m_tankBATexture, m_tankMatrix.m, 0, 0, 0, -5, -2);
+	
+
+	//m_tankMatrix.m[6] = m_tankX;
+	//m_tankMatrix.m[7] = m_tankY;
+
+	
+	//m_2dRenderer->drawSpriteTransformed3x3(m_tankBATexture, m_tankMatrix.m, 0, 0, 0, m_tankMatrix.m31, m_tankMatrix.m32);
+
 	// output some text, uses the last used colour
 	char fps[32];
+	char TankT[32];
 	sprintf_s(fps, 32, "FPS: %i", getFPS());
 	m_2dRenderer->drawText(m_font, fps, 0, 720 - 32);
-	m_2dRenderer->drawText(m_font, "Press Space for sound!", 0, 720 - 64);
-
+	
 	// done drawing sprites
+	
 	m_2dRenderer->end();
+
 }
--- project2D/Application2D.h
+++ project2D/Application2D.h
@@ -3,7 +3,12 @@
 #include "Application.h"
 #include "Renderer2D.h"
 #include "Audio.h"
-#include "Vector2.h"
+#include <Vector2.h>
+#include <Vector3.h>
+#include <Vector4.h>
+#include <Matrix2.h>
+#include <Matrix3.h>
+#include <Matrix4.h>
 
 class Application2D : public aie::Application {
 public:
@@ -20,11 +25,18 @@ public:
 protected:
 
 	aie::Renderer2D*	m_2dRenderer;
+	aie::Renderer2D*	m_TankRenderer;
+	
 	aie::Texture*		m_texture;
 	aie::Texture*		m_shipTexture;
 	aie::Font*			m_font;
 	aie::Audio*			m_audio;
 
+	aie::Texture*		m_tankTexture;
+	aie::Texture*		m_tankBATexture;
+	Matrix3<float>      m_tankMatrix;
+
 	float m_cameraX, m_cameraY;
+	float m_tankX, m_tankY, m_tankR;
 	float m_timer;
 };
